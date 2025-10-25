204d67cce59d068f7b8e1187fd32dc3ec40389f5
julxpo <j.poyasnyuk@yandex.ru>
2025-10-25 15:17:37 +0300

    Creating a registration page and connecting to the database

---
+++ app/src/main/java/com/example/shop/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.shop
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val userLogin: EditText = findViewById(R.id.user_login)
+        val userEmail: EditText = findViewById(R.id.user_email)
+        val userPass: EditText = findViewById(R.id.user_pass)
+        val button: Button = findViewById(R.id.button_reg)
+
+        button.setOnClickListener{
+            val login = userLogin.text.toString().trim()
+            val email = userEmail.text.toString().trim()
+            val pass = userPass.text.toString().trim()
+
+            if(login == "" || email == "" || pass == "")
+                Toast.makeText(this, "Не все поля заполнены", Toast.LENGTH_LONG).show()
+            else{
+                val user = User(login, email, pass)
+
+                val db = DBHelper(this, null)
+                db.addUser(user)
+                Toast.makeText(this, "Пользователь $login добавлен", Toast.LENGTH_LONG).show()
+
+                userLogin.text.clear()
+                userEmail.text.clear()
+                userPass.text.clear()
+            }
+        }
+
+    }
+}
