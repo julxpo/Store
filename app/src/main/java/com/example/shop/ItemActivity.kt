8688d60089dea79ec1ebb3b086d6035c625a6b08
julxpo <j.poyasnyuk@yandex.ru>
2025-11-07 16:54:53 +0300

    added a window for viewing detailed product information

---
+++ app/src/main/java/com/example/shop/ItemActivity.kt
@@ -0,0 +1,24 @@
+package com.example.shop
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ItemActivity : AppCompatActivity() {
+    //@SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_item)
+
+
+        val title: TextView = findViewById(R.id.item_list_title_one)
+        val text: TextView = findViewById(R.id.item_list_text)
+
+        title.text = intent.getStringExtra("itemTitle")
+        text.text = intent.getStringExtra("itemText")
+    }
+}
--- app/src/main/java/com/example/shop/ItemsAdapter.kt
+++ app/src/main/java/com/example/shop/ItemsAdapter.kt
@@ -1,9 +1,11 @@
 package com.example.shop
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 
@@ -16,6 +18,7 @@ class ItemsAdapter(var items: List<Item>, var context: Context) : RecyclerView.A
         val title: TextView = view.findViewById(R.id.item_list_title)
         val desc: TextView = view.findViewById(R.id.item_list_desc)
         val price: TextView = view.findViewById(R.id.item_list_price)
+        val btn: Button = view.findViewById(R.id.item_list_button)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
@@ -39,5 +42,14 @@ class ItemsAdapter(var items: List<Item>, var context: Context) : RecyclerView.A
         )
 
         holder.image.setImageResource(imageId)
+
+        holder.btn.setOnClickListener {
+            val intent = Intent(context, ItemActivity::class.java)
+
+            intent.putExtra("itemTitle", items[position].title)
+            intent.putExtra("itemText", items[position].text)
+
+            context.startActivity(intent)
+        }
     }
 }
